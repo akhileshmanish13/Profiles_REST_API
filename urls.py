6d911044dfda02f395b0d646855a3baf13a4be7a
Akhilesh Rawat <40234473+akhileshmanish13@users.noreply.github.com>
2019-01-16 19:46:39 +0530

    Final commit

--- urls.py
+++ urls.py
@@ -1,18 +1,23 @@
-from django.conf.urls import url
+"""profiles_project URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
 from django.conf.urls import include
-
-from rest_framework.routers import DefaultRouter
-
-from . import views
-
-router = DefaultRouter()
-router.register('hello-viewset', views.HelloViewSet, base_name='hello-viewset')
-router.register('profile', views.UserProfileViewSet)
-router.register('login', views.LoginViewSet, base_name= 'login')
-router.register('feed', views.UserProfileFeedViewSet)
+from django.conf.urls import url
 
 urlpatterns = [
-    url(r'^hello-view/', views.HelloApiView.as_view()),
-    url(r'', include(router.urls))
-
+    url(r'^admin/', admin.site.urls),
+    url(r'^api/', include('profile_api.urls'))
 ]
